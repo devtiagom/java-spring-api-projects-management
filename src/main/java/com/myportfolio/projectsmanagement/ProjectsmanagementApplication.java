7ce7988a02d313768ee632b3c29dc0a591403b70
Tiago Machado <tiago.o.machado@gmail.com>
2020-05-28 16:34:08 -0300

    initial commit

---
+++ src/main/java/com/myportfolio/projectsmanagement/ProjectsmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.myportfolio.projectsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myportfolio/projectsmanagement/ProjectsmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myportfolio.projectsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
