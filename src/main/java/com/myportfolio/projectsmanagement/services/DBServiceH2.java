73a4083807b3dd5c56390031fab600a34d4f6986
devtiagom <tiago.o.machado@gmail.com>
2020-06-16 16:35:48 -0300

    created jpa repository to entity Developer, and some seeds on init

--- src/main/java/com/myportfolio/projectsmanagement/services/DBServiceH2.java
+++ src/main/java/com/myportfolio/projectsmanagement/services/DBServiceH2.java
@@ -1,7 +1,11 @@
 package com.myportfolio.projectsmanagement.services;
 
+import com.myportfolio.projectsmanagement.domain.DeveloperDomain;
 import com.myportfolio.projectsmanagement.domain.ProjectDomain;
+import com.myportfolio.projectsmanagement.domain.enums.DeveloperPosition;
+import com.myportfolio.projectsmanagement.repositories.DeveloperRepository;
 import com.myportfolio.projectsmanagement.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -10,9 +14,12 @@ import java.util.Arrays;
 public class DBServiceH2 {
 
     private final ProjectRepository projectRepository;
+    private final DeveloperRepository developerRepository;
 
-    public DBServiceH2(ProjectRepository projectRepository) {
+    @Autowired
+    public DBServiceH2(ProjectRepository projectRepository, DeveloperRepository developerRepository) {
         this.projectRepository = projectRepository;
+        this.developerRepository = developerRepository;
     }
 
     public void instantiateTestDatabase() {
@@ -20,5 +27,10 @@ public class DBServiceH2 {
         ProjectDomain project02 = new ProjectDomain("CRM Empresa Y", "Desenvolvimento de sistema CRM da empresa Y.");
         ProjectDomain project03 = new ProjectDomain("E-Commerce", "Desenvolvimento de sistema E-Commerce.");
         this.projectRepository.saveAll(Arrays.asList(project01, project02, project03));
+
+        DeveloperDomain dev01 = new DeveloperDomain("John Doe", DeveloperPosition.JUNIOR);
+        DeveloperDomain dev02 = new DeveloperDomain("Mark Z.", DeveloperPosition.ENGINEER);
+        DeveloperDomain dev03 = new DeveloperDomain("Linus T.", DeveloperPosition.MANAGER);
+        this.developerRepository.saveAll(Arrays.asList(dev01, dev02, dev03));
     }
 }
